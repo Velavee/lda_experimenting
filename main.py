3c40de178752448e581d7bbfd51170dcdf055a35
Velavee <josephsg@oregonstate.edu>
2022-12-11 19:54:00 -0800

    chore: save wip

--- main.py
+++ main.py
@@ -11,25 +11,23 @@ from text_processing import *
 
 def main():
     text_data = []
-    with open("dataset.csv") as f: # Most examples seem to use .csv. Why?
+    with open("dataset.csv") as f:
         for line in f:
             if line != '\n':
                 line = re.sub(r'[^\w\s]', '', line)
                 tokens = prepare_text_for_lda(line)
-                print(tokens)
-                text_data.append(tokens)
-                # if random.random() > .99: # What is the purpose of this?
-                    # print(tokens)
-                    # text_data.append(tokens)
+                if random.random() > .99:
+                    print(tokens)
+                    text_data.append(tokens)
 
         f.close()
 
     dictionary = corpora.Dictionary(text_data)
     corpus = [dictionary.doc2bow(text) for text in text_data]
-    pickle.dump(corpus, open('corpus.pkl', 'wb')) # Where is this corpus from? Can I use other corpora?
+    pickle.dump(corpus, open('corpus.pkl', 'wb'))
     dictionary.save('dictionary.gensim')
 
-    NUM_TOPICS = 3 # Does altering the number of topics change the relevance of results?
+    NUM_TOPICS = 5
     ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics = NUM_TOPICS, id2word=dictionary, passes=15)
     ldamodel.save('model5.gensim')
 
@@ -38,9 +36,9 @@ def main():
         print(topic)
 
     # Visualization - wasn't working
-    # import pyLDAvis.gensim
-    # lda_display = pyLDAvis.gensim.prepare(ldamodel, corpus, dictionary, sort_topics=False)
-    # pyLDAvis.display(lda_display)
+    import pyLDAvis.gensim_models
+    lda_display = pyLDAvis.gensim_models.prepare(ldamodel, corpus, dictionary, sort_topics=False)
+    pyLDAvis.display(lda_display)
 
 if __name__ == '__main__':
     main()
